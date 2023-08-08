cfffb43d72c6ffb9d6ad6b2c9b34c6f8c312b05c
Elisabeth Yolanda Christin <elisabeth.christin@xlplanet.co.id>
2023-08-08 08:45:32 +0700

    first commimt

---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,82 @@
+<template>
+    <footer class="mt-5">
+        <div class="container pt-5 pb-4">
+            <div class="row align-items-center px-4 gx-lg-4 g-md-5 gy-5">
+                <div class="col-lg-4 col-md-6 order-lg-0 order-1">
+                    <div class="hstack align-items-center flex-wrap gap-3 pe-5">
+                        <router-link to="/" class="font-weight-700 font-size-16 text-decoration-none text-dark">
+                            Home
+                        </router-link>
+                        <router-link to="/" class="font-weight-700 font-size-16 text-decoration-none text-dark">
+                            Product
+                        </router-link>
+                        <router-link to="/" class="font-weight-700 font-size-16 text-decoration-none text-dark">
+                            Contact Us
+                        </router-link>
+                    </div>
+                    <div class="mt-2">
+                        <router-link to="/become-a-customer" class="btn btn-p-grey-57 text-p-black font-weight-600 font-size-13 me-3 hover-bg-p-orange-9 px-4 py-2 rounded-0">
+                            Become a Customer
+                        </router-link>
+                        <router-link to="/become-a-supplier" class="btn btn-p-grey-57 text-p-black font-weight-600 font-size-13 hover-bg-p-orange-9 px-4 py-2 rounded-0">
+                            Become a Supplier
+                        </router-link>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 order-0 orderlg-1">
+                    <div class="text-center">
+                        <div>
+                            <img style="height: 55px;" src="/assets/images/logo-espn.png" />
+                        </div>
+                        <div class="">
+                            <img style="height: 45px;" src="/assets/images/elevenis-biz-black.png" />
+                            <img class="pt-3" style="height: 38px;" src="/assets/images/nustantrara-black.png" />
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 ms-lg-auto order-2 orderlg-2">
+                    <table class="navbar-contact font-weight-400 font-size-12">
+                        <tr>
+                            <td>
+                                <img src="/assets/images/icons/location.svg" />
+                            </td>
+                            <td>
+                                Wisma 46-Kota BNI, Lantai 39, Jl. Jendral Sudirman Kav. 1
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <img class="me-2" src="/assets/images/icons/call.svg" />
+                            </td>
+                            <td>
+                                02139371122
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <img class="me-2" src="/assets/images/icons/mail.svg" />
+                            </td>
+                            <td>
+                                enusantara@elevenia.co.id
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <!-- <div class="col-12">
+                    <div class="text-center mt-3 font-weight-400 font-size-12">
+                        © 2022 espn all rights reserved
+                    </div>
+                </div> -->
+            </div>
+        </div>
+        <div class="text-center my-2 font-weight-400 font-size-12">
+            © 2022 espn all rights reserved
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
---
+++ resources/js/pages/Home/Index.vue
@@ -0,0 +1,230 @@
+<template>
+  <section class="hero">
+    <div class="position-relative">
+      <img src="/assets/images/hero-bg.png" style="width: 100%;" />
+      <div>
+        <div class="position-absolute text-center text-white w-100 header-text">
+          <div class="container-lg">
+            <div class="row justify-content-between align-items-center w-100">
+              <div class="col-4 text-start">
+                <div
+                  class="font-weight-700 font-size-xl-40 font-size-lg-36 font-size-md-30 font-size-sm-20 font-size-10"
+                >
+                  Welcome
+                </div>
+              </div>
+              <div class="col-8 text-end">
+                <div
+                  class="font-size-xl-21 font-size-lg-18 font-size-md-16 font-size-sm-10 font-size-6 font-weight-400"
+                >
+                  We <span class="font-weight-700">Focus</span> to help local
+                  businesses <br />
+                  that specify the products on raw materials local comodities
+                  <br />
+                  and ingredient platforms to empower <br />
+                  and expand sales through domestic and global markets
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="product">
+    <div class="image-hero position-relative">
+      <img src="/assets/images/product-bg.png" style="width: 100%;" />
+      <div class="container">
+        <div
+          class="position-absolute text-center text-white"
+          style="top: 50%; left: 15%; transform: translate(-50%, -50%);"
+        >
+          <div
+            class="py-md-4 px-md-5 px-sm-4 py-sm-3 px-3 py-2"
+            style="
+              filter: drop-shadow(4px 4px 4px rgba(0, 0, 0, 0.25));
+              border: 3px solid white;
+            "
+          >
+            <div
+              class="font-size-xl-40 font-size-lg-36 font-size-md-20 font-size-10 font-weight-700 text-center text-p-orange-12"
+            >
+              New <br />
+              Product
+            </div>
+          </div>
+        </div>
+        <div
+          class="position-absolute"
+          style="top: 50%; left: 67%; transform: translate(-50%, -50%);"
+        >
+          <div class="d-flex justify-content-between list-product">
+            <div class="product-item">
+              <div class="position-relative product-item-image">
+                <div class="bg-p-white">
+                    <img src="/assets/images/cengkeh-gapokan.png" />
+                </div>
+                <div
+                  class="img-product-overlay d-flex align-items-center pe-md-4 ps-md-2 pe-1"
+                >
+                  <p
+                    class="text-p-white font-weight-600 text-center font-size-lg-12 font-size-md-10 font-size-4"
+                  >
+                    Fuli Kelurahan Bunga Harapan Luwu Timur
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="product-item">
+              <div class="position-relative product-item-image">
+                <div class="bg-p-white">
+                    <img src="/assets/images/full-lpig.png" />
+                </div>
+                <div
+                  class="img-product-overlay d-flex align-items-center pe-md-4 ps-md-2 pe-1"
+                >
+                  <p
+                    class="text-p-white font-weight-600 text-center font-size-lg-12 font-size-md-10 font-size-4"
+                  >
+                    Fuli Kelurahan Bunga Harapan Luwu Timur
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="product-item">
+              <div class="position-relative product-item-image">
+                <div class="bg-p-white">
+                    <img src="/assets/images/pala-abcd.png" />
+                </div>
+                <div
+                  class="img-product-overlay d-flex align-items-center pe-md-4 ps-md-2 pe-1"
+                >
+                  <p
+                    class="text-p-white font-weight-600 text-center font-size-lg-12 font-size-md-10 font-size-4"
+                  >
+                    Fuli Kelurahan Bunga Harapan Luwu Timur
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="">
+      <swiper :navigation="true" :modules="modules" class="mySwiper">
+        <swiper-slide>
+          <img src="/assets/images/slider-home-01.png" class="w-100" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="/assets/images/slider-home-02.png" class="w-100" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="/assets/images/slider-home-3.png" class="w-100" />
+        </swiper-slide>
+      </swiper>
+    </div>
+  </section>
+  <section class="container mt-5">
+    <div
+      class="font-size-28 font-weight-600 text-p-green-20 text-center"
+      style="text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);"
+    >
+      Category Product
+    </div>
+    <div class="row align-items-start py-5 g-5">
+      <div class="col-lg-3 col-md-4 col-sm-6">
+        <div style="min-width: 220px;">
+          <div class="">
+            <img
+              src="/assets/images/spices.png"
+              class="w-100 object-fit-cover"
+              style="height: 220px; min-width: 290px;"
+            />
+          </div>
+          <div class="bg-p-green-20 py-3 px-4 text-center">
+            <div class="font-weight-700 text-p-white font-size-18">
+              Spices
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-4 col-sm-6">
+        <div style="min-width: 220px;">
+          <div class="">
+            <img
+              src="/assets/images/fishery.png"
+              class="w-100 object-fit-cover"
+              style="height: 220px; min-width: 290px;"
+            />
+          </div>
+          <div class="bg-p-green-20 py-3 px-4 text-center">
+            <div class="font-weight-700 text-p-white font-size-18">
+              Fish Meal
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-4 col-sm-6">
+        <div style="min-width: 220px;">
+          <div class="">
+            <img
+              src="/assets/images/palm_oil.png"
+              class="w-100 object-fit-cover"
+              style="height: 220px; min-width: 290px;"
+            />
+          </div>
+          <div class="bg-p-green-20 py-3 px-4 text-center">
+            <div class="font-weight-700 text-p-white font-size-18">
+              Palm Oil
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-4 col-sm-6">
+        <div style="min-width: 220px;">
+          <div class="">
+            <img
+              src="/assets/images/chinese-med.png"
+              class="w-100 object-fit-cover"
+              style="height: 220px; min-width: 290px;"
+            />
+          </div>
+          <div class="bg-p-green-20 py-3 px-4 text-center">
+            <div class="font-weight-700 text-p-white font-size-18">
+              Chinese Med
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+
+export default {
+  name: "Home",
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [Navigation],
+    };
+  },
+};
+</script>
+<style>
+.swiper-button-next:after,
+.swiper-button-prev:after {
+  color: white;
+}
+</style>
